f582b8f25d300b6a1fb47c2eeae31e9d6b2b34ba
entegral <e2robby@gmail.com>
2019-03-06 20:33:52 -0800

    update profile component to use imported css objects

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,28 +2,18 @@ import React from 'react';
 import UserProfileDisplay from './UserProfileDisplay';
 import UserOrgDisplay from './UserOrgDisplay';
 import UserJobDisplay from './UserJobDisplay';
+import { listStyleParent } from './helpers/jsStyleObjects';
 
 function Profile () {
 
-  const componentStyle = {
-    display: 'flex',
-    justifyContent: 'space-around',
-    flexWrap: 'wrap'
-  }
-
-  const divColumnStyle = {
-    margin: '10px',
-    color: 'red'
-    // border: '1px solid grey',
-  }
 
   return (
-    <div style={componentStyle}>
-      <UserProfileDisplay style={divColumnStyle}/>
+    <div style={listStyleParent}>
+      <UserProfileDisplay/>
 
-      <UserOrgDisplay style={divColumnStyle}/>
+      <UserOrgDisplay/>
       
-      <UserJobDisplay style={divColumnStyle} />
+      <UserJobDisplay/>
       
     </div>
   );
