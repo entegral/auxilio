9dda4f1e34fe37ebe49215bdfcc5d0695b0007ce
entegral <e2robby@gmail.com>
2019-03-08 13:09:36 -0800

    auxilio api returns all or single user

--- src/apis/auxilioServerApi.js
+++ src/apis/auxilioServerApi.js
@@ -1,7 +1,9 @@
 
-export function getUserData(uid) {
+export function getUserData(uid = '') {
   let jsonResponse;
-  return fetch('http://localhost:3001/users/').then((response)=>{
+  let url_string;
+  uid ? url_string = `1?uid=${uid}` : url_string = '';
+  return fetch(`http://localhost:3001/users/${url_string}`).then((response)=>{
     jsonResponse = response.json();
     console.log(jsonResponse);
     return jsonResponse;
